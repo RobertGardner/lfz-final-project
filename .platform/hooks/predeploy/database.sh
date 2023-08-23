4f62aa5173d086300a89ac10fd1f014c3c688851
Robert Gardner <rdgardner.dev@gmail.com>
2023-08-23 02:54:52 +0000

    Provide easy mechanism to keep from replacing database

--- .platform/hooks/predeploy/database.sh
+++ .platform/hooks/predeploy/database.sh
@@ -3,6 +3,10 @@
 # Install psql
 dnf -y install postgresql15
 
+# Uncomment the next line if you do _not_ want the database to be deleted and replaced
+# on every deploy. Remember to comment it if you _do_ want the database updated.
+# exit 0
+
 # Create a connection URL of form `postgresql://user:pwd@hostspec/dbname`
 DB_HOSTNAME=`/opt/elasticbeanstalk/bin/get-config environment -k RDS_HOSTNAME`
 DB_PORT=`/opt/elasticbeanstalk/bin/get-config environment -k RDS_PORT`
